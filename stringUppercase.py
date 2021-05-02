ea42443320d74d62f0f79604220c5903f9be5f4a
Satripleen <satripleen@gmail.com>
2021-05-01 21:48:29 -0400

    create merge conflict

--- stringUppercase.py
+++ stringUppercase.py
@@ -1,5 +1,8 @@
 def stringUpperCase(string):
     string1 = ''
+    a = ""
+    c = ""
+    b = ""
     for i in range(len(string)):
         if (string[i] >= 'a' and string[i] <= 'z'):
             string1 = string1 + chr((ord(string[i]) - 32))
