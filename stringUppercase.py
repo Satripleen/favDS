e1c39c46cc516d8554dcd68fd64b6f8055d56e58
Satripleen <satripleen@gmail.com>
2021-05-01 20:01:37 -0400

    first commit

---
+++ stringUppercase.py
@@ -0,0 +1,10 @@
+def stringUpperCase(string):
+    string1 = ''
+    for i in range(len(string)):
+        if (string[i] >= 'a' and string[i] <= 'z'):
+            string1 = string1 + chr((ord(string[i]) - 32))
+        else:
+            string1 = string1 + string[i]
+    return string1
+
+print(stringUpperCase("Satripleen"))
