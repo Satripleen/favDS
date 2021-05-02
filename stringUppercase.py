b069a60e6a1d2f563dd15cad8701acdf3dfc4ab3
Satripleen <satripleen@gmail.com>
2021-05-01 20:18:31 -0400

    use upper to capitalize the string

--- stringUppercase.py
+++ stringUppercase.py
@@ -1,10 +1,4 @@
 def stringUpperCase(string):
-    string1 = ''
-    for i in range(len(string)):
-        if (string[i] >= 'a' and string[i] <= 'z'):
-            string1 = string1 + chr((ord(string[i]) - 32))
-        else:
-            string1 = string1 + string[i]
-    return string1
+    return string.upper()
 
 print(stringUpperCase("Satripleen"))
